8998925c048f7491021c798a4578d018a45a0176
paddi0010 <kontakt.paddi001@gmail.com>
2023-12-08 03:32:35 +0100

    sources

---
+++ src/index.js
@@ -0,0 +1,58 @@
+document.getElementById('newTodo').addEventListener('keyup', function(event) {
+    if (event.key === 'Enter') {
+        addTodo();
+    }
+});
+
+function addTodo() {
+    var todoText = document.getElementById('newTodo').value;
+    var newTodoInput = document.getElementById('newTodo');
+    if (todoText === '') {
+        alert('Bitte eine Aufgabe eingeben!');
+        newTodoInput.style.border = '2px solid red';
+        return;
+    }
+
+    var todoList = document.getElementById('todoList');
+    var li = document.createElement('li');
+    li.className = 'todoItem';
+
+    var span = document.createElement('span');
+    span.innerText = todoText;
+
+    var deleteButton = document.createElement('button');
+    deleteButton.innerText = 'Löschen';
+    deleteButton.onclick = function() {
+        todoList.removeChild(li);
+    };
+
+    var completeButton = document.createElement('button');
+    completeButton.innerText = 'Abgeschlossen';
+    completeButton.onclick = function() {
+        setStatus(li, 'abgeschlossen');
+    };
+
+    var inProgressButton = document.createElement('button');
+    inProgressButton.innerText = 'In Arbeit';
+    inProgressButton.onclick = function() {
+        setStatus(li, 'inArbeit');
+    };
+
+    li.appendChild(span);
+    li.appendChild(deleteButton);
+    li.appendChild(completeButton);
+    li.appendChild(inProgressButton);
+    todoList.appendChild(li);
+
+    document.getElementById('newTodo').value = '';
+}
+
+function setStatus(element, status) {
+    element.classList.remove('abgeschlossen', 'inArbeit');
+    element.classList.add(status);
+}
+
+function clearTodo() {
+    var todoList = document.getElementById('todoList');
+    todoList.innerHTML = '';
+}
